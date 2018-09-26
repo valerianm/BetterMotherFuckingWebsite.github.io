90a18f2ca55fcafef38f6155447a4deb493cf56d
valerian.m <valerian24@hotmail.fr>
2018-09-26 16:14:02 +0200

    Popup done

---
+++ scripts.js
@@ -0,0 +1,6 @@
+popup = function() {
+  if (confirm('Venez voir notre site de livraison de saucisson !')) {
+    window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  }
+}
+window.setTimeout(popup, 10000)
