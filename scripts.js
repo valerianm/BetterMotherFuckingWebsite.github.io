85beaa8ab4a92fad1b0e44c5a0bd5d6c3c5660cf
valerian.m <valerian24@hotmail.fr>
2018-09-26 21:51:50 +0200

    Psy button without fonts

--- scripts.js
+++ scripts.js
@@ -3,4 +3,18 @@ popup = function() {
     window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
   }
 }
-window.setTimeout(popup, 10000)
+window.setTimeout(popup, 10000);
+
+var linker = document.querySelector('link');
+var PsyButton = document.querySelector('button');
+
+PsyButton.onclick = function() {
+  var style = linker.getAttribute('href');
+    if(style === 'style.css') {
+      linker.setAttribute ('href','PsyStyle.css');
+      PsyButton.textContent = ('Go back to normal')
+    } else {
+      linker.setAttribute ('href','style.css');
+      PsyButton.textContent = ('Go Psychedelic')
+    }
+}
